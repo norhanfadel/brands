61468d262d1adf02ed31be096e12de60d784a575
nour <nourhan.6050@gmail.com>
2020-04-16 22:03:52 +0200

    registration servlet

---
+++ src/main/java/com/brands/servlets/Registration.java
@@ -0,0 +1,70 @@
+package com.brands.servlets;
+
+import com.brands.dao.Users;
+import com.brands.dto.UserImp;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Registration extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+
+        RequestDispatcher dispatcher =request.getRequestDispatcher("login.jsp");
+        dispatcher.forward(request,response);
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+
+        System.out.println("post");
+        String name = request.getParameter("name");
+        String email = request.getParameter("emailRegistration");
+        String phone = request.getParameter("phone");
+
+       String password = request.getParameter("passwordRegistration");
+        String address = request.getParameter("address");
+        System.out.println("address-----"+address+name+email+phone);
+        String code = request.getParameter("welcomeCode");
+        System.out.println("code-----"+code);
+        Double welcomeCode = Double.parseDouble(code);
+        System.out.println("password is=" + password);
+
+        Users user = new Users();
+        user.setStatus("OFFLINE");
+        user.setUserRole("USER");
+        user.setPhone(phone);
+        user.setUserName(name);
+        user.setAddress(address);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setCreditLimit(welcomeCode);
+        UserImp userImp = new UserImp();
+        if (userImp.register(user)) {
+
+            System.out.println("valid");
+            response.sendRedirect("login.jsp");
+
+        } else {
+            String paramName1 = "This Account is aleadry Exist!";
+            System.out.println("from else");
+            request.setAttribute("register", paramName1);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+            dispatcher.forward(request, response);
+
+
+        }
+    }
+
+}
