5273cf074fc26b1ddd55b015d484514fb369b3df
shimaa1091993 <shimaamohamed1091993@gmail.com>
2020-04-17 15:36:49 +0200

    edit in login.jsp

--- src/main/java/com/brands/servlets/Registration.java
+++ src/main/java/com/brands/servlets/Registration.java
@@ -55,25 +55,27 @@ public class Registration extends HttpServlet {
         user.setPhone(phone);
         UserImp userImp = new UserImp();
         List<Users> usersList = userImp.getAllUsers();
-        for (Users userss : usersList) {
+        if(name != null && email != null && password !=null && phone !=null && address != null && code !=null) {
+            for (Users userss : usersList) {
 
-            if (userss.getEmail().equals(email)) {
-                String paramName1 = "This Account is aleadry Exist!";
-                System.out.println("from else");
-                request.setAttribute("register", paramName1);
+                if (userss.getEmail().equals(email)) {
+                    String paramName1 = "This Account is aleadry Exist!";
+                    System.out.println("from else");
+                    request.setAttribute("register", paramName1);
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+                    dispatcher.forward(request, response);
+
+                }
+            }
+
+            if (userImp.register(user)) {
+                System.out.println("valid");
+                String paramName2 = "true1";
+                request.setAttribute("true1", paramName2);
                 RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
                 dispatcher.forward(request, response);
 
             }
-        }
-
-        if (userImp.register(user)) {
-            System.out.println("valid");
-            String paramName2 = "true1";
-            request.setAttribute("true1", paramName2);
-            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
-            dispatcher.forward(request, response);
-
         }
 //        } else {
 //            String paramName1 = "This Account is aleadry Exist!";
