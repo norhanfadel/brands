d1d146712c94ef24445cf8174627c7ab0332b852
badri <mohamedbadri136@gmail.com>
2020-04-17 18:11:21 +0200

    OrderDetails class implementation

--- src/main/java/com/brands/dto/Test.java
+++ src/main/java/com/brands/dto/Test.java
@@ -1,29 +1,32 @@
 package com.brands.dto;
 
+import com.brands.dao.Category;
 import com.brands.dao.Products;
 import com.brands.dao.Users;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Date;
 import java.util.List;
 
 public class Test {
 
+
     static Session session = MySessionFactory.getMySession();
     public static void main(String[] args) {
 
-        Users user = new Users();
+//        Users user = new Users();
 //        session.beginTransaction();
 //       session.persist(cart);
 //        session.persist(user);
 //        session.getTransaction().commit();
 
-        UserImp userImp = new UserImp();
-        Users userX = userImp.getUserById(userImp.getUserIdByMail(user.getEmail()));
-        userX.setAddress("cairo");
-        boolean isItTrue = userImp.updateUser(userX);
-        System.out.println(isItTrue);
+//        UserImp userImp = new UserImp();
+//        Users userX = userImp.getUserById(userImp.getUserIdByMail(user.getEmail()));
+//        userX.setAddress("cairo");
+//        boolean isItTrue = userImp.updateUser(userX);
+//        System.out.println(isItTrue);
 
         // user=(Users) session.get(Users.class,7);
 //        if(!userImp.login(user)){
@@ -36,7 +39,7 @@ public class Test {
 //        Users user1 = (Users) session.get(Users.class, 3);
 //        userImp.addCredit(creditAdding.getCode(), user1);
 //        // userImp.register(user,cart);
-        System.out.println("done");
+//        System.out.println("done");
 //
 //        Products products = new Products(2, user1.getCart(), user1, 2, 500);
 //        session.beginTransaction();
@@ -44,7 +47,28 @@ public class Test {
 //        session.getTransaction().commit();
 //        userImp.updateCreditWhenBuying(user1);
 //        System.out.println("done");
+        badriTest();
+        System.out.println("done");
+    }
 
+    public static void badriTest(){
+        System.out.println("inside");
+        Category category = new Category("t-shirt");
+//        session.beginTransaction();
+//        session.persist(category);
+//        session.getTransaction().commit();
+        ProductDto productDto = new ProductImp();
+//        List<Products> allProducts = productDto.getAllProducts();
+//        System.out.println("after");
+//        System.out.println(allProducts.size());
+//        for(Products product : allProducts){
+//            System.out.println("inside loop ");
+//            System.out.println(product.getName() + " badri");
+//        }
+
+        Products product = productDto.addProduct(new Products(category, new Date(), "jeans", 12));
+
+        System.out.println(product.getName());
 
     }
 }
