09db0b591450c84cc83089e1925128c7658b96da
shimaa1091993 <shimaamohamed1091993@gmail.com>
2020-04-18 22:15:44 +0200

    home.js

---
+++ src/main/webapp/js/home.js
@@ -0,0 +1,39 @@
+
+function showHomeProduct() {
+    $("#home").css("display","block");
+    $("#men").css("display","none");
+    $("#women").css("display","none");
+    $("#kids").css("display","none");
+
+}
+function showMenProduct() {
+    debugger
+    $("#home").css("display","none");
+    $("#men").css("display","block");
+    $("#women").css("display","none");
+    $("#kids").css("display","none");
+    $("#menLink").css("background-color","#fdb45e");
+    $("#womenLink").css("background-color","white");
+    $("#kidsLink").css("background-color","white");
+
+}
+function showWomenProduct() {
+    $("#home").css("display","none");
+    $("#men").css("display","none");
+    $("#women").css("display","block");
+    $("#kids").css("display","none");
+    $("#menLink").css("background-color","white");
+    $("#womenLink").css("background-color","#fdb45e");
+    $("#kidsLink").css("background-color","white");
+
+}
+function showKidsProduct() {
+    $("#home").css("display","none");
+    $("#men").css("display","none");
+    $("#women").css("display","none");
+    $("#kids").css("display","block");
+    $("#menLink").css("background-color","white");
+    $("#womenLink").css("background-color","white");
+    $("#kidsLink").css("background-color","#fdb45e");
+
+}
