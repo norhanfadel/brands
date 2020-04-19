d54e5296c609277bea28cce97b97b99a93a388c5
nour <nourhan.6050@gmail.com>
2020-04-19 03:38:25 +0200

    cookies.js

---
+++ src/main/webapp/js/cookies.js
@@ -0,0 +1,56 @@
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var user=getCookie("username");
+  if (user != "") {
+  
+ 	     document.getElementById("email").value =user ;
+   
+	
+  }
+}
+function checkCookie1() {
+
+	 if (document.getElementById('remember').checked) {
+     user = document.getElementById("email").value;
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }}
+	 else{
+		//  setCookie("username", "", 30);
+		deleteCookie("username","",30);
+	
+	 }
+	
+	
+  }
+
+function deleteCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*-24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+ 
+}
